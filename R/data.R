faaf9939a90719246bb521a532898699ea7afced
Lincoln <lincoln@colling.net.nz>
2020-09-19 14:45:16 +0100

    updated metadata
    updated DESCRIPTION to include correct package meta data

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Dummy survey data
+#' @format a tibble with 25 rows and 12 columns
+"survey_data"
+
+
+#' Stars Wars data
+#'
+#' A subset of the starwars data set from the dplyr package
+"starwars2"
+
